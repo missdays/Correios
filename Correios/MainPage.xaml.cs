57446b478bbfda46d256c3afa9c6369513ca457f
Érik Lima <erik@suasvendas.com>
2022-04-30 14:41:55 -0300

    Primeiro Commit do Projeto

---
+++ Correios/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Correios.Servico.Modelo;
+using Correios.Servico;
+
+namespace Correios
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //TODO - validações 
+
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep)) {
+                try {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if (end != null)
+                    {
+
+                        RESULTADO.Text = string.Format("Endereço: {2} {3} {0}, {1}", end.localidade, end.uf, end.logradouro, end.bairro, end.cep);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o CEP informado: " + cep, "OK");
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "OK");
+                }
+            }
+        }
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP invalido! O CEP deve conter 8 caracteres.", "OK");
+
+                valido = false;
+            }
+            int NovoCEP = 0;
+            if (!int.TryParse(cep, out NovoCEP))
+            {
+                DisplayAlert("ERRO", "CEP invalido! O CEP deve conter apenas números.", "OK");
+
+                valido = false;
+            }
+
+            return valido;
+        }
+    }
+}
---
+++ Correios/Servico/ViaCEPServico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using Correios.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace Correios.Servico
+{
+    public class ViaCEPServico
+    {
+            private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+            public static Endereco BuscarEnderecoViaCEP(string cep)
+         {
+            String NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+
+
+          }
+        
+    }
+} 
